26491f5f116209aa3396f572295c2b4e0392f13a
sahlminkok <sahlminkok7@gmail.com>
2023-11-10 06:55:40 +0100

    Create menu component to render all pizzas

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Pizza from "./Pizza";
+
+export const Menu = () => {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza />
+    </main>
+  );
+};
