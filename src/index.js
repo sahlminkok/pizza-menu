b1fc50af4b2244df7e0dabbb5011bb1f7b672abe
sahlminkok <sahlminkok7@gmail.com>
2024-08-18 17:58:51 +0100

    Conditionally rendering class name

--- src/index.js
+++ src/index.js
@@ -93,12 +93,12 @@ function Menu() {
 
 function Pizza({ pizzaObject }) {
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObject.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObject.photoName} alt={pizzaObject.name} />
       <div>
         <h3>{pizzaObject.name}</h3>
         <p>{pizzaObject.ingredients}</p>
-        <span>{pizzaObject.price}</span>
+        <span>{pizzaObject.soldOut ? "SOLD OUT" : pizzaObject.price}</span>
       </div>
     </li>
   );
