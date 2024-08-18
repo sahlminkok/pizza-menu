b017ccff43a6e7481acfb05f31b05960d0f13d18
sahlminkok <sahlminkok7@gmail.com>
2024-08-18 11:47:14 +0100

    Add multilple components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,49 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello, World</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>Fast React Pizza Co.</h1>;
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <div>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return <footer>We are currently {isOpen ? "open" : "close"}</footer>;
+}
+
+export default function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/margherita.jpg" alt="margherita" />
+      <h2>Pizza Margherita</h2>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
